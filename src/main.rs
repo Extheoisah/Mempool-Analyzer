a514fe7e44b73fbaa6f23051781f99c770903c9c
Theophilus <isahtheophilus@gmail.com>
2022-04-14 20:56:17 +0100

    added function to derive and send mempool data to client

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ extern crate lazy_static;
 extern crate rocket;
 extern crate bitcoincore_rpc;
 
-use bitcoincore_rpc::{
-    bitcoin::{self, BlockHash},
-    Auth, Client, Error, RpcApi,
-};
+use bitcoincore_rpc::{bitcoin::Txid, json, Auth, Client, RpcApi};
+
 use rocket::{serde::json::Json, Build, Rocket};
 use serde::Serialize;
+use std::collections::HashMap;
 // use serde_json::{json, Result};
 
 lazy_static! {
@@ -29,7 +28,7 @@ struct StatusMessage {
 
 #[derive(Debug, Serialize)]
 struct MemData {
-    txn_ids: Vec<bitcoincore_rpc::bitcoin::Txid>,
+    txn_ids: HashMap<Txid, json::GetMempoolEntryResult>,
 }
 
 #[get("/")]
@@ -38,25 +37,38 @@ fn index() -> &'static str {
 }
 
 #[get("/mempool-data")]
-fn fetch_mempool_data() -> &'static str {
-    "All, data!"
+fn fetch_mempool_data() -> Json<MemData> {
+    // Send "All" mempool data to endpoint @localhost:8000
+    let datum: MemData = get_mempool_txns().unwrap();
+    Json(datum)
 }
 
-fn get_mempool_txns() {
+fn get_mempool_txns() -> Result<MemData, String> {
     // Check if a wallet is loaded in bitcoin core,
     // if loaded,
     // Get all transaction ID's in the mempool
-    let wallets = RPC.list_wallets().unwrap();
-    println!("All wallets: {:?}", wallets);
+    let wallets: Result<Vec<String>, bitcoincore_rpc::Error> = RPC.list_wallets();
+    match wallets {
+        Ok(wallet) => {
+            println!("All wallets: {:?}", wallet)
+        }
+        Err(_) => return Err(String::from("Failed to fetch wallets")),
+    }
 
-    let mem_pool_data = MemData {
-        txn_ids: RPC.get_raw_mempool().unwrap(),
-    };
-    println!("mempool data: {:#?}", mem_pool_data);
+    let raw_mem_pool: Result<HashMap<Txid, json::GetMempoolEntryResult>, bitcoincore_rpc::Error> =
+        RPC.get_raw_mempool_verbose();
+
+    match raw_mem_pool {
+        Ok(data) => {
+            println!("mempool data: {:#?}", (data));
+            Ok(MemData { txn_ids: data })
+        }
+        Err(_) => return Err("No data found! {}".into()),
+    }
 }
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    get_mempool_txns();
+    //computes all routes for launch to server
     rocket::build().mount("/", routes![index, fetch_mempool_data])
 }
