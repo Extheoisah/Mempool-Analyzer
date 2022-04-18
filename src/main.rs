a29b33c832461f65032e187aa70a34b626047455
Theophilus <isahtheophilus@gmail.com>
2022-04-18 12:32:31 +0100

    server routes for fetching single txn  data complete

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@ extern crate lazy_static;
 extern crate rocket;
 extern crate bitcoincore_rpc;
 
-use bitcoincore_rpc::{bitcoin::Txid, json, Auth, Client, RpcApi};
+use bitcoincore_rpc::{
+    bitcoin::{hash_types::Txid, hashes::sha256d::Hash},
+    json::{self, GetMempoolEntryResult},
+    Auth, Client, RpcApi,
+};
 
-use rocket::{serde::json::Json, Build, Rocket};
-use serde::Serialize;
-use std::collections::HashMap;
+use rocket::http::Header;
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    serde::json::Json,
+    Build, Rocket,
+};
+use rocket::{Request, Response};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, vec};
 // use serde_json::{json, Result};
 
 lazy_static! {
@@ -21,6 +31,28 @@ lazy_static! {
     // TO-DO: Allow users to load and unload wallet
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[derive(Serialize)]
 struct StatusMessage {
     message: String,
@@ -31,9 +63,14 @@ struct MemData {
     txn_ids: HashMap<Txid, json::GetMempoolEntryResult>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct TxnInput {
+    txn_id: String,
+}
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Welcome!"
 }
 
 #[get("/mempool-data")]
@@ -67,8 +104,26 @@ fn get_mempool_txns() -> Result<MemData, String> {
     }
 }
 
+#[post("/txn-data", data = "<txn_id>", format = "application/json")]
+fn fetch_txn_data(txn_id: Json<TxnInput>) -> Json<json::GetMempoolEntryResult> {
+    let id: String = txn_id.txn_id.clone();
+    let txnid_hash: Hash = id.parse().unwrap();
+    let tx_id: Txid = Txid::from_hash(txnid_hash);
+    let data: GetMempoolEntryResult = RPC.get_mempool_entry(&tx_id).unwrap();
+    Json(data)
+}
+
+#[post("/tx-id", data = "<txn_id>", format = "application/json")]
+fn fetch_txn(txn_id: Json<TxnInput>) -> Json<TxnInput> {
+    let txid: String = txn_id.txn_id.clone();
+    Json(TxnInput { txn_id: txid })
+}
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     //computes all routes for launch to server
-    rocket::build().mount("/", routes![index, fetch_mempool_data])
+    rocket::build().attach(CORS).mount(
+        "/",
+        routes![index, fetch_mempool_data, fetch_txn_data, fetch_txn],
+    )
 }
